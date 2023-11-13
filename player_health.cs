0573f85355d3a3984418396fdf6f64d7bb545916
Syfreia#7203 <91333171+Syfreia@users.noreply.github.com>
2023-11-13 10:16:41 +0100

    Update player_health.cs

--- player_health.cs
+++ player_health.cs
@@ -1,6 +1,7 @@
 using System.collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class player_health : MonoBehaviour  {
     public int Health = 1;
@@ -15,14 +16,29 @@ public class player_health : MonoBehaviour  {
     // update is called  once per frame
     void update () {
         if (gameObject.transform.position.y < -7) {
-            Debug.Log("Player Has Died");
+            //Debug.Log("Player Has Died");
             isDead = true;
+        }
+        if (isDead == true) {
+            StartCoroutine(Die());
         }
 
         // Can be used to check if player is dead if it has more than 1 health
         /*if (Health <= 0) {
             isDead = true;
         }*/
+    }
+
+    IEnumerator Die() {
+        // play death animation
+        /*Debug.Log("Player Has fallen off the map");
+        yield return new WaitForSeconds(2);
+        Debug.Log("Player Has Died");*/
+
+        // respawn player
+        SceneManager.LoadScene("Level1"); // Level1 is the name of the scene
+        yield return null;
+        
     }
     
 }
